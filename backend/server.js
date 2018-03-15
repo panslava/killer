6cabaad7ecf291d32bafbc8151d0c3aa715c7d2b
panslava <VeryCrazySlava@gmail.com>
2018-03-16 01:31:52 +0300

    http?

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var https = require('https');
+var http = require('http');
 var app = express();
 
 app.use(express.static('../frontend/build'));
-app.listen(80);
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
