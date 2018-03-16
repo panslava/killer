5b06b645b062ff859af061c6f3e261e5b073d942
SvetlanaTselikova <svetacelikova1@gmail.com>
2018-03-16 21:26:20 +0300

    creating register.vue, creating register form without design

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id='app' >
+    <form >
+            <input type='text' v-model='name'>
+            <br>
+            <input type='text' v-model='surName'>
+            <br>
+            <input type='text' v-model='course'>
+            <br>
+            <input type='text' v-model='email'>
+            <br>
+            <input type='text' v-model='vk'>
+            <br>
+            <input type='file' v-model='photo'>
+            <br>
+            <button type='submit' @click='register'>Зарегистрироваться</button>
+
+    </form>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'Register',
+  data: function () {
+    return {
+      name:'',
+      surName:'',
+      course='',
+      email='',
+      vk='',
+      photo=''
+    }
+  },
+
+  methods: {
+    register: function () {
+
+
+    }
+  }
+
+    }
+
+</script>
