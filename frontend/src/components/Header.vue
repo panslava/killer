1c3a1eaa6d3735de8d697637ccfe0234f1d00be8
anatolyefimov <efimo.tolik@gmail.com>
2018-03-27 00:24:00 +0300

    added fonts and variable

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class = 'header'>
+        <div>{{content}}</div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ['content']
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/utils/var.scss';
+
+    .header {
+        display:flex;
+        background-color: inherit;
+        color: $font-color--main;
+        font-weight: $font-weight--thin;
+        font-family: $font-family--header;
+        font-size: $font-size--header;
+        justify-content: center;
+    }
+</style>
+
+
+
